4ae5bf2e3157ee06b4cc667a0ae3f8e9a28c26df
Oaju <leewangju0902@gmail.com>
2023-08-25 11:06:17 +0900

    데이터 분석 연습 ( pandas)

--- DemoThread01.py
+++ DemoThread01.py
@@ -3,16 +3,22 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class Worker(QThread): #Back Ground Thread로..
+    a =0
     def run(self):
         while True:
-            print("안녕하세요")
-            self.sleep(1)
+            a+=1
+            print("안녕하세요{0}",a)
+            self.msleep(10)
 
 class Worker2(QThread): #Back Ground Thread로..
+    a =0
     def run(self):
         while True:
-            print("WORK2")
-            self.sleep(2)
+            a+=1
+            if(a>10):
+                print("안녕하세요{0}",a)
+                a=0
+            self.msleep(10)
 
 class MyWindow(QMainWindow):
     def __init__(self):
